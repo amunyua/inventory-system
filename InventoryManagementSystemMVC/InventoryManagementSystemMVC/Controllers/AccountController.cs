128c76530b0d706acd8f0a132eca797baab4f023
amunyua <amunyua10@gmail.com>
2017-03-09 13:34:34 +0300

    templates added

--- InventoryManagementSystemMVC/InventoryManagementSystemMVC/Controllers/AccountController.cs
+++ InventoryManagementSystemMVC/InventoryManagementSystemMVC/Controllers/AccountController.cs
@@ -392,7 +392,7 @@ namespace InventoryManagementSystemMVC.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Dashboard");
         }
 
         //
@@ -449,7 +449,7 @@ namespace InventoryManagementSystemMVC.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Dashboard");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
